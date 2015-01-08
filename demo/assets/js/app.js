351640d2709ef10edf4715bc8e044501c3712219
Song Yang <songyang@thoughtworks.com>
2015-01-08 09:58:30 +0800

    extract inline script to individual one

---
+++ demo/assets/js/app.js
@@ -0,0 +1,41 @@
+(function (l) {
+    var L = new l();
+
+    var data = {
+        about: "This about A Mobile Framework For Romantic",
+        what: "A Framework",
+        why: "Why is a new Framework."
+    };
+
+    var about = function () {
+        var result = L.tmpl("<h3>{%=o.about%}</h3>", data);
+        document.getElementById("results").innerHTML = result;
+    };
+
+    function what() {
+        var result = L.tmpl("<h3>{%=o.what%}</h3>", data);
+        document.getElementById("results").innerHTML = result;
+    }
+
+    function why() {
+        var result = L.tmpl("<h3>{%=o.why%}</h3>", data);
+        document.getElementById("results").innerHTML = result;
+    }
+
+    function home() {
+        L.Router.navigate("/");
+    }
+
+    L.Router
+        .add(/#/, home)
+        .add(/#about/, about)
+        .add(/#what/, what)
+        .add(/#why/, why)
+        .load();
+
+    L.Event.on('tick', function (results) {
+        console.log(results);
+    });
+    console.log(L.Router.routes);
+
+}(lettuce));
