1fa27fdde941bc02cf084762b2efa5317c891945
Song Yang <songyang@thoughtworks.com>
2015-01-09 00:12:25 +0800

    helper | add one more testcase for extend

--- specs/helpers-spec.js
+++ specs/helpers-spec.js
@@ -1,25 +1,21 @@
 'use strict';
 
 describe("Helper", function () {
-    var L;
-
-    beforeEach(function () {
-        L = new lettuce();
-    });
+    var l = lettuce;
 
     it("should be a object", function () {
-        expect(lettuce.isObject([])).toEqual(true);
-        expect(lettuce.isObject([{}])).toEqual(true);
+        expect(l.isObject([])).toEqual(true);
+        expect(l.isObject([{}])).toEqual(true);
     });
 
     describe("Extend", function () {
         it("should be able to extend object", function () {
-	        var results = lettuce.extend({one: 1}, {two: 2});
-            expect(results).toEqual({ one: 1, two: 2 });
+            expect(l.extend({one: 1}, {two: 2})).toEqual({ one: 1, two: 2 });
+            expect(l.extend({two: 1}, {two: 2})).toEqual({two: 2 });
         });
 
         it("should be unable to extend object when no a object", function () {
-            var results = lettuce.extend("", { two: 2 } );
+            var results = l.extend("", { two: 2 });
             expect(results).toEqual("");
         });
     });
