300c626c6fb43fc960ecec19720700afff1ac8bf
Fengda HUANG <h@phodal.com>
2015-01-15 08:49:00 +0800

    Add simple parser

--- dist/lettuce.js
+++ dist/lettuce.js
@@ -108,6 +108,36 @@ Parser.prototype.init = function () {
 
 };
 
+var DSLRunner = {
+    run: function(methods) {
+        this.ingredients = [];
+        this.methods     = methods;
+
+        this.executeAndRemove('first');
+
+        for (var key in this.methods) {
+            if (key !== 'last' && key.match(/^bake/)) {
+                this.executeAndRemove(key);
+            }
+        }
+
+        this.executeAndRemove('last');
+    },
+
+    addIngredient: function(ingredient) {
+        this.ingredients.push(ingredient);
+    },
+
+    executeAndRemove: function(methodName) {
+        var output = this.methods[methodName]();
+        delete(this.methods[methodName]);
+        return output;
+    }
+};
+
+Parser.prototype = Lettuce.extend(Parser.prototype, DSLRunner);
+
+
 var parser = {
     Parser: Parser
 };
--- specs/parser-spec.js
+++ specs/parser-spec.js
@@ -1,13 +1,14 @@
 'use strict';
 
 describe("Parser", function () {
-    var L;
+    var L, Parser;
 
     beforeEach(function() {
         L = new lettuce();
+        Parser = new L.Parser();
     });
 
     it('it must be a function ot Lettuce', function () {
-        
+
     });
 });
