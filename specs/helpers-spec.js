76d5a29acb977fbb6757ed45301f0edf14daa046
Fengda HUANG <h@phodal.com>
2015-01-16 12:14:01 +0800

    Helper | add test for helper.defaults

--- specs/helpers-spec.js
+++ specs/helpers-spec.js
@@ -10,13 +10,37 @@ describe("Helper", function () {
 
     describe("Extend", function () {
         it("should be able to extend object", function () {
-            expect(l.extend({one: 1}, {two: 2})).toEqual({ one: 1, two: 2 });
-            expect(l.extend({two: 1}, {two: 2})).toEqual({two: 2 });
+            expect(l.extend({one: 1}, {two: 2})).toEqual({one: 1, two: 2});
+            expect(l.extend({two: 1}, {two: 2})).toEqual({two: 2});
         });
 
         it("should be unable to extend object when no a object", function () {
-            var results = l.extend("", { two: 2 });
+            var results = l.extend("", {two: 2});
             expect(results).toEqual("");
         });
     });
+
+    describe("Defaults", function () {
+        it("should be return variable when same key", function () {
+            var origin = {one: 1};
+            var new_object = {one: 2};
+            l.defaults(origin, new_object);
+            expect(origin).toEqual({one: 1});
+        });
+
+        it("should return empty string when defaults is empty string", function () {
+            var emptyString = "";
+            var defaults = {two: 2};
+            l.defaults(emptyString, defaults);
+            expect(emptyString).toEqual("");
+        });
+
+        it("should return defaults when defaults is empty object", function () {
+            var empty = {};
+            var defaults = {two: 2};
+            l.defaults(empty, defaults);
+            expect(empty).toEqual({two: 2});
+        });
+    });
+
 });
