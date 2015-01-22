0708b2ee96d7a40255be3d14d13ae91a3faa0170
Fengda HUANG <h@phodal.com>
2015-01-22 08:40:56 +0800

    Effect | add simple effect

--- dist/lettuce.js
+++ dist/lettuce.js
@@ -328,6 +328,32 @@ var simpleView = {
 Lettuce.prototype = Lettuce.extend(Lettuce.prototype, simpleView);
 
 
+var Effect = {
+    slideDown: function (element, duration, finalheight) {
+        var s = element.style;
+        s.height = '0px';
+
+        var y = 0;
+        var frameRate = 10;
+        var totalFrames = duration / frameRate;
+        var heightIncrement = finalheight / totalFrames;
+        var oneSecond = 1000;
+        var interval = oneSecond / frameRate;
+        var tween = function () {
+            y += heightIncrement;
+            s.height = y + 'px';
+            if (y < finalheight) {
+                setTimeout(tween, interval);
+            }
+        };
+        tween();
+    }
+};
+
+
+Lettuce.prototype = Lettuce.extend(Lettuce.prototype, Effect);
+
+
 /*
  *Inspired by http://krasimirtsonev.com/blog/article/A-modern-JavaScript-router-in-100-lines-history-api-pushState-hash-url
  *  Backbone
---
+++ src/effect.js
@@ -0,0 +1,24 @@
+var Effect = {
+    slideDown: function (element, duration, finalheight) {
+        var s = element.style;
+        s.height = '0px';
+
+        var y = 0;
+        var frameRate = 10;
+        var totalFrames = duration / frameRate;
+        var heightIncrement = finalheight / totalFrames;
+        var oneSecond = 1000;
+        var interval = oneSecond / frameRate;
+        var tween = function () {
+            y += heightIncrement;
+            s.height = y + 'px';
+            if (y < finalheight) {
+                setTimeout(tween, interval);
+            }
+        };
+        tween();
+    }
+};
+
+
+Lettuce.prototype = Lettuce.extend(Lettuce.prototype, Effect);
