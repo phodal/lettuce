59455b102e173cf4ad4245dfccf321d2021e3299
Fengda HUANG <h@phodal.com>
2015-01-27 09:24:43 +0800

    Aajax | remove load method

--- dist/lettuce.js
+++ dist/lettuce.js
@@ -157,10 +157,6 @@ Lettuce.get = function (url, callback) {
     Lettuce.send(url, 'GET', callback);
 };
 
-Lettuce.load = function (url, callback) {
-    Lettuce.send(url, 'GET', callback);
-};
-
 Lettuce.post = function (url, data, callback) {
     Lettuce.send(url, 'POST', callback, data);
 };
--- src/ajax.js
+++ src/ajax.js
@@ -2,10 +2,6 @@ Lettuce.get = function (url, callback) {
     Lettuce.send(url, 'GET', callback);
 };
 
-Lettuce.load = function (url, callback) {
-    Lettuce.send(url, 'GET', callback);
-};
-
 Lettuce.post = function (url, data, callback) {
     Lettuce.send(url, 'POST', callback, data);
 };
