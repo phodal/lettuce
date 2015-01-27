924b6dedb0b07277024ea0d1e2ae9ddf05d75617
Fengda HUANG <h@phodal.com>
2015-01-27 08:54:02 +0800

    Ajax | add load test

--- specs/ajax-spec.js
+++ specs/ajax-spec.js
@@ -24,6 +24,24 @@ describe("Ajax", function () {
         });
     });
 
+    it("specifying html when you need it", function (done) {
+        var doneFn = jasmine.createSpy("success");
+
+        lettuce.load('/some', function (result) {
+            expect(result).toEqual("<h2>fsasfA</h2>");
+            done();
+        });
+
+        expect(jasmine.Ajax.requests.mostRecent().url).toBe('/some');
+        expect(doneFn).not.toHaveBeenCalled();
+
+        jasmine.Ajax.requests.mostRecent().respondWith({
+            "status": 200,
+            "contentType": 'text/plain',
+            "responseText": '<h2>fsasfA</h2>'
+        });
+    });
+
 
     it("should be post to some where", function (done) {
         var doneFn = jasmine.createSpy("success");
